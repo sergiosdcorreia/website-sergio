db85fc21dc1e5a706db85ab5d4934e61bb7f9610
Sergio Correia <sergiosdcorreia@gmail.com>
2021-01-29 21:53:05 +0000

    Home page photo and text

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,12 @@
 <template>
-  <q-page class="flex flex-center">
-    HomePage
+  <q-page class="flex flex-center bg-grey-10">
+    <q-avatar size="150px">
+      <img src="SergioFoto200.png">
+    </q-avatar>
+    <div>
+      <p class="text-white text-h3 q-ml-md">Sergio Correia</p>
+      <p class="text-white text-h5 q-ml-md">Creative Frontend Developer</p>
+    </div>
   </q-page>
 </template>
 
